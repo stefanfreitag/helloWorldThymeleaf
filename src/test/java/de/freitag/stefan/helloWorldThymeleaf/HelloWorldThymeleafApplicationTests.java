c6987127f784a95b3883cddeee6f9248610597f5
Stefan Freitag <stefan.freitag@udo.edu>
2020-02-09 15:26:31 +0100

    Initial commit

---
+++ src/test/java/de/freitag/stefan/helloWorldThymeleaf/HelloWorldThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package de.freitag.stefan.helloWorldThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
